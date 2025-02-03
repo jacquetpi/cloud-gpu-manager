be19e21290b14d4b74cdb415940c33a3d2509c7b
Pierre Jacquet <jacquetpi@users.noreply.github.com>
2025-02-03 14:40:19 -0500

    trying to simplify code

--- exp-mig.py
+++ exp-mig.py
@@ -2,7 +2,7 @@ from monitoring import *
 from gpu_settings import *
 from workloads import *
 
-import sys
+import sys, time
 
 #########################
 # Create MIG instances  #
@@ -11,40 +11,35 @@ def iterate_on_gi(mig_wrapper, monitors_wrapper, suitable_gpus):
     gi_profile_list = mig_wrapper.list_gpu_instance_profiles(gpu_id=suitable_gpus[0]) # We assume homogeneity on GPUs
     for gi_profile in gi_profile_list:
         if gi_profile['free_instances'] > 0:
+
             print('Creating', gi_profile['name'], 'on all GPUs')
-
-            # I) Create GIs on all GPUs
-            for suitable_gpu in suitable_gpus: 
-                mig_wrapper.create_gpu_instance(gpu_id=suitable_gpu, gi_profiles=gi_profile['name']) # Create MIG instance
-                list_gi_active = mig_wrapper.list_gpu_instance_active(gpu_id=suitable_gpu)
-                if not list_gi_active:  # Check if everything went well
-                    print('GI creation: Something went wrong on GPU', suitable_gpu)
-
-            # II) Iterate on all CIs profile
-            iterate_on_ci(mig_wrapper, monitors_wrapper, suitable_gpus)
-
-            # III) Destroy all GIs
-            for suitable_gpu in suitable_gpus: 
-                for gi_active in list_gi_active:
-                    mig_wrapper.destroy_gpu_instance(gpu_id=suitable_gpu)
+            iterate_on_ci(mig_wrapper, monitors_wrapper, suitable_gpus, gi_profile['name'])
 
 #############################
 # Create a Compute instance #
 #############################
-def iterate_on_ci(mig_wrapper, monitors_wrapper, suitable_gpus):
-    # Again, we assume homogeneity on GPUs and takes GPU0 as a referential
+def iterate_on_ci(mig_wrapper, monitors_wrapper, suitable_gpus, protagonist_gi):
+
+    # First, we have to create a mig to retrieve the associate list of CI profile
+    mig_wrapper.create_gpu_instance(gpu_id=suitable_gpus[0], gi_profiles=protagonist_gi)
     list_gi_active = mig_wrapper.list_gpu_instance_active(gpu_id=suitable_gpus[0])
     ci_profile_list = mig_wrapper.list_compute_instance_profiles(gpu_id=suitable_gpus[0], gi_id=list_gi_active[0]['gi_id']) # At that time, there is only one GI on GPU0
+    mig_wrapper.destroy_gpu_instance(gpu_id=suitable_gpus[0])
 
+    # Then we iterate on CI profiles
     for ci_profile in ci_profile_list:
+
+        # I) Create GIs on all GPUs
+        for suitable_gpu in suitable_gpus:
+            mig_wrapper.create_gpu_instance(gpu_id=suitable_gpu, gi_profiles=protagonist_gi) # Create MIG instance
 
         # II) iterate_on_complements
         iterate_on_complements(mig_wrapper, monitors_wrapper, suitable_gpus, list_gi_active[0]['name'], protagonist_ci=ci_profile['name'])
 
         # III) Destroy all CIs
-        for suitable_gpu in suitable_gpus: 
-            for gi_active in list_gi_active:
-                mig_wrapper.destroy_compute_instance(gpu_id=suitable_gpu)
+        for suitable_gpu in suitable_gpus:
+            mig_wrapper.destroy_compute_instance(gpu_id=suitable_gpu)
+            mig_wrapper.destroy_gpu_instance(gpu_id=suitable_gpu)
 
 #########################
 #Complementary instances#
@@ -56,17 +51,22 @@ def iterate_on_complements(mig_wrapper, monitors_wrapper, suitable_gpus, protago
         list_gi_active_specific = [gi for gi in mig_wrapper.list_gpu_instance_active(gpu_id=suitable_gpu) if gi['name'] == protagonist_gi] # We now may have multiple GIs due to iteration
         mig_wrapper.create_compute_instance(gpu_id=suitable_gpu, gi_id=list_gi_active_specific[0]['gi_id'], ci_profiles=protagonist_ci) # Create Compute instance
 
+    # Capture idle
+    setting_name = protagonist_gi.replace(' ','_') + '|' + protagonist_ci.replace(' ','_') + '|idle'
+    monitors_wrapper.update_monitoring({'context': setting_name}, monitor_index=0, reset_launch=True)
+    time.sleep(180)
+
     first_round = True
     last_round = False
     index=0
-    # Then the complements
+    # Then test on load
     while True:
 
         # I) Create complement CIs on all GIs (the increasing workload)
-        if not first_round:  
+        if not first_round:
             for suitable_gpu in suitable_gpus:
                 smallest_profile = mig_wrapper.list_gpu_instance_profiles(gpu_id=suitable_gpu)[0]
-                if smallest_profile['free_instances'] > 0: 
+                if smallest_profile['free_instances'] > 0:
                     mig_wrapper.create_gpu_instance(gpu_id=suitable_gpu, gi_profiles=smallest_profile['name'], create_ci=True)
 
         first_round = False
@@ -88,7 +88,7 @@ def iterate_on_complements(mig_wrapper, monitors_wrapper, suitable_gpus, protago
 
 #############################
 # Launch stress on CIs      #
-#############################             
+#############################
 def launch_stress(mig_wrapper, monitors_wrapper, suitable_gpus, mig_list):
     workloads = []
     for mig in mig_list:
@@ -99,6 +99,7 @@ def launch_stress(mig_wrapper, monitors_wrapper, suitable_gpus, mig_list):
     for workload in workloads:
         workload.wait()
 
+# Also measure idle
 if __name__ == "__main__":
 
     print('Starting MIG experiment')
@@ -142,8 +143,12 @@ if __name__ == "__main__":
     ##########################
     # Starting  measurements #
     ##########################
-    monitors_wrapper.update_monitoring({'context':'init'}, monitor_index=0, reset_launch=False)
 
+    print('Capturing idle')
+    monitors_wrapper.update_monitoring({'context':'idle'}, monitor_index=0, reset_launch=False)
+    time.sleep(180)
+    print('Idle capture ended')
+    
     try:
         monitors_wrapper.start_monitoring()
         iterate_on_gi(mig_wrapper, monitors_wrapper, suitable_gpus)
